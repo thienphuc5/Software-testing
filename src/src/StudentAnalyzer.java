28e8947b670c9e6dce770ddd316c323f4a0c45cd
thienphuc5 <phucnguyenkid27@gmail.com>
2026-01-11 21:36:41 +0700

    feat: implement calculateValidAverage() #2

--- src/src/StudentAnalyzer.java
+++ src/src/StudentAnalyzer.java
@@ -17,4 +17,25 @@ public class StudentAnalyzer {
         }
         return count;
     }
+    public double calculateValidAverage(List<Double> scores) {
+        if (scores == null || scores.isEmpty()) {
+            return 0;
+        }
+
+        double sum = 0;
+        int count = 0;
+
+        for (double score : scores) {
+            if (score >= 0 && score <= 10) {   // validate
+                sum += score;
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            return 0;
+        }
+
+        return sum / count;
+    }
 }
